c1607df79f6150525f9938272eb01faf9fda5cb6
Grant Forsythe <grantwforsythe@gmail.com>
2022-10-02 21:56:33 -0400

    Update section2

--- section2/app.js
+++ section2/app.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 const UTF_8 = 'utf8';
+const BASE_PATH = path.parse('section2/');
 
 const callback = (error) => {
     if (error) {
@@ -14,27 +16,45 @@ const callback = (error) => {
 const server = http.createServer((request, response) => {
     const json = JSON.stringify(request.headers, null, 4);
     // timestamp in seconds
-    let timestamp = Math.floor(Date.now() / 1000);
-
-    fs.writeFile(`section2/data/request-${timestamp}.json`, json, UTF_8, callback);
+    const timestamp = Math.floor(Date.now() / 1000);
+    
+    fs.writeFile(
+        path.join(BASE_PATH, 'data', `request-${timestamp}.json`),
+        json, UTF_8, callback
+    );
 
     switch (request.url) {
         case '/':
-            if (request.method === 'get')
+            if (request.method === 'GET')
             {
                 response.setHeader('Content-Type', 'application/json');
-            } else if (request.method === 'post') {
-                console.log(request);
+                response.end(json);
+            } else if (request.method === 'POST') {
+                // The on method allows you to listen to certain events
+                // The event will trigger when a new buffer is ready to be read
+                const body = [];
+                request.on('data', (chuck) => {
+                    body.push(chuck);
+                });
+
+                // After all the chucks have been read in
+                request.on('end', () => {
+                    const parsedBody = Buffer.concat(body).toString();
+                    const username = parsedBody.split('&')[0].split('=')[1];
+                    const password = parsedBody.split('&')[1].split('=')[1];
+                    console.log(`Username: ${username}, Password: ${password}`);
+                });
+                response.write('<html><body><h1>Post Request</h1></body></html>')
+                response.end();
             }
-            response.end(json);
             break;
-        case '/form':
+        case '/login':
             response.writeHead(200);
-            response.end(fs.readFileSync('section2/templates/form.html'));
+            response.end(fs.readFileSync(path.join(BASE_PATH, 'templates', 'form.html')));
             break;
         default:
             response.writeHead(404);
-            response.end(fs.readFileSync('section2/templates/404.html'));
+            response.end(fs.readFileSync(path.join(BASE_PATH, 'templates', '404.html')));
     }
 });
 
